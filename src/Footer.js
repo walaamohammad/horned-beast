40b27e8ab119d8da1040aab45c353007447b6fd0
walaamohammad <layan_abadi86@yahoo.com>
2021-08-01 22:05:10 +0300

    add css

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+ class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer>
+                    <p>
+                       walaa bani mfarrej &copy;2021
+                    </p>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Footer
