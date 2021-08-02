4a8e0530de9c633a774e3f67e841e554b04556a9
walaamohammad <layan_abadi86@yahoo.com>
2021-08-02 22:21:30 +0300

    add

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
  class HornedBeasts extends Component {
+     constructor(){
+         super();
+         this.state={
+             click:props.click
+         }
+     }
+     riseBeats=(click)=>{
+        
+         this.state({
+            click:this.state.click+1
+         })
+     }
     render() {
+
+       
         return (
             <div>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.image_url}/>
+                <img src={this.props.image_url} alt ={this.props.title} width='400px' height='400px'/>
                 <p>{this.props.description}</p>
-            </div>
+                <img onclick={()=>{this.riseBeats}} src="https://celebrant-reginasmall.com.au/wp-content/uploads/2014/12/heart-icon-300x261.png" width='30px' height='30px'/>
+               <p>{this.state.click}</p> 
+                  </div>
         )
     }
 }
